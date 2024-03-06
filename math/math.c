2c4be1801b52493b0814eab91b91513c2cda835a
Maël Ehrhard <mael.ehrhard@outlook.fr>
2024-03-06 11:38:45 +0100

    ✨ Add rotate function

--- math/math.c
+++ math/math.c
@@ -13,3 +13,13 @@ double clamp(double value, double min, double max) {
     }
     return value;
 }
+
+double degrees_to_radians(double degrees) { return degrees * M_PI / 180; }
+
+double radians_to_degrees(double radians) { return radians * 180 / M_PI; }
+
+void rotate_by_angle(double distance, double angle, Vector *vector) {
+    const double a = degrees_to_radians(angle);
+    vector->x = distance * cos(a);
+    vector->y = distance * sin(a);
+}
--- math/math.h
+++ math/math.h
@@ -5,6 +5,7 @@
 typedef struct Point Point;
 typedef struct Circle Circle;
 typedef struct Rectangle Rectangle;
+typedef Point Vector;
 
 struct Point {
     double x;
@@ -24,5 +25,8 @@ struct Rectangle {
 
 double distance(Point p1, Point p2);
 double clamp(double value, double min, double max);
+double degrees_to_radians(double degrees);
+double radians_to_degrees(double radians);
+void rotate_by_angle(double distance, double angle, Vector *vector);
 
 #endif // ARKANOID_MATH_H
