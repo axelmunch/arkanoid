7d7f664dd332f3629f4e56955594edcec9b52ecf
Maël Ehrhard <mael.ehrhard@outlook.fr>
2024-03-20 14:04:45 +0100

    👔 Add entity creation methods - magic numbers to put in a separate file

---
+++ entity.c
@@ -0,0 +1,56 @@
+#include "entity.h"
+
+AnimatedEntity createEntity(SpecificType type, Point position) {
+    AnimatedEntity entity;
+    switch (type) {
+    case CAPSULE_SLOW:
+    case CAPSULE_CATCH:
+    case CAPSULE_EXPAND:
+    case CAPSULE_DIVIDE:
+    case CAPSULE_LASER:
+    case CAPSULE_BREAK:
+    case CAPSULE_ADDITION: {
+        entity = createCapsule();
+        break;
+    }
+    case HARMFUL_1:
+    case HARMFUL_2:
+    case HARMFUL_3: {
+        entity = createHarmful(type);
+        break;
+    }
+    }
+    entity.time_before_next_animation = ANIMATION_TIMER_MS;
+    entity.velocity = 1.5;
+    entity.direction = 270; // corresponds to -90 deg
+    entity.current_animation = 1;
+    entity.specific_type = type;
+    entity.hit_box.origin = position;
+    return entity;
+}
+AnimatedEntity createHarmful(SpecificType harmfulType) {
+    AnimatedEntity harmful;
+    harmful.type = HARMFUL;
+    switch (harmfulType) {
+    case HARMFUL_1:
+        harmful.max_animation = 8;
+        break;
+    case HARMFUL_2:
+        harmful.max_animation = 11;
+        break;
+    default:
+        harmful.max_animation = 24;
+        break;
+    }
+    harmful.hit_box.width = 32;
+    harmful.hit_box.height = 32;
+    return harmful;
+}
+AnimatedEntity createCapsule() {
+    AnimatedEntity capsule;
+    capsule.type = CAPSULE;
+    capsule.max_animation = 8;
+    capsule.hit_box.width = 32;
+    capsule.hit_box.height = 16;
+    return capsule;
+}
--- entity.h
+++ entity.h
@@ -1,8 +1,24 @@
 #ifndef ARKANOID_ENTITY_H
 #define ARKANOID_ENTITY_H
 
+#include "config.h"
 #include "math/math.h"
-typedef enum { Harmful, Capsule } Type;
+typedef struct AnimatedEntity AnimatedEntity;
+typedef struct Ball Ball;
+typedef struct VAUS VAUS;
+typedef enum { HARMFUL, CAPSULE, EXPLOSION } EntityType;
+typedef enum {
+    CAPSULE_SLOW,
+    CAPSULE_CATCH,
+    CAPSULE_EXPAND,
+    CAPSULE_DIVIDE,
+    CAPSULE_LASER,
+    CAPSULE_BREAK,
+    CAPSULE_ADDITION,
+    HARMFUL_1,
+    HARMFUL_2,
+    HARMFUL_3
+} SpecificType;
 
 struct AnimatedEntity {
     int max_animation;
@@ -10,7 +26,8 @@ struct AnimatedEntity {
     int time_before_next_animation;
     double velocity;
     double direction;
-    Type type;
+    EntityType type;
+    SpecificType specific_type;
     Rectangle hit_box;
 };
 
@@ -23,6 +40,10 @@ struct Ball {
 struct VAUS {
     int expand_size;
     Rectangle hit_box;
+    SpecificType active_powerup;
 };
 
+AnimatedEntity createEntity(SpecificType type, Point position);
+AnimatedEntity createHarmful(SpecificType harmfulType);
+AnimatedEntity createCapsule();
 #endif // ARKANOID_ENTITY_H
